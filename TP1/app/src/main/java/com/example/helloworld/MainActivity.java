227d5b06f9c485c4975c60ce30619fe0228de139
Wying96 <ying.wu@utbm.fr>
2019-09-19 12:27:33 +0200

    TP1

--- TP1/app/src/main/java/com/example/helloworld/MainActivity.java
+++ TP1/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+public Button btnHello;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnHello=findViewById(R.id.btnHello);
+        btnHello.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,HelloActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
