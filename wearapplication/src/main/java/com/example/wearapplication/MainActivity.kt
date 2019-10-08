32ece2b7ee6dccd32115cfbaec005136ce20e22a
leo68.schwartz <leo68.schwartz@gmail.com>
2019-10-08 15:06:48 +0200

    first interface

--- wearapplication/src/main/java/com/example/wearapplication/MainActivity.kt
+++ wearapplication/src/main/java/com/example/wearapplication/MainActivity.kt
@@ -2,12 +2,22 @@ package com.example.wearapplication
 
 import android.os.Bundle
 import android.support.wearable.activity.WearableActivity
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : WearableActivity() {
+    lateinit var mText: TextView
+    private val btnOk: Button = findViewById(R.id.btn_ok)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mText = findViewById(R.id.text)
+        btnOk.setOnClickListener {
+            Toast.makeText(this@MainActivity, "You clicked", Toast.LENGTH_SHORT).show()
+        }
 
         // Enables Always-on
         setAmbientEnabled()
