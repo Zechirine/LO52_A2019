65747b62c176fd792a8cd8da96c4b440a8adc057
Xuzhipeng <shuxuzhipeng@outlook.com>
2019-11-23 00:52:30 +0100

    Spprime Helloworld

--- HelloWorld/Android.mk
+++
@@ -1,7 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := core.c descriptor.c io.c os/darwin_usb.c os/linux_usbfs.c
-LOCAL_C_INCLUDES += $(LOCAL_PATH) $(LOCAL_PATH)/os
-LOCAL_MODULES := libusb
-LOCAL_MODULE_TAGS := optional
-include $(BUILD_SHARED_LIBRARY)
