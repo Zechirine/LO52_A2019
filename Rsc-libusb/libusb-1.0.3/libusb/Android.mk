3973d0adc5b8221c5b311040bf2eda8d993fd209
pujol <killian.pujol@utbm.fr>
2019-12-30 14:22:21 +0100

    Avancement TP3

--- Rsc-libusb/libusb-1.0.3/libusb/Android.mk
+++ Rsc-libusb/libusb-1.0.3/libusb/Android.mk
@@ -1,6 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES := core.c descriptor.c io.c os/darwin_usb.c os/linux_usbfs.c
+LOCAL_SRC_FILES := core.c descriptor.c io.c sync.c os/darwin_usb.c os/linux_usbfs.c
 LOCAL_C_INCLUDES += $(LOCAL_PATH) $(LOCAL_PATH)/os
 LOCAL_MODULES := libusb
 LOCAL_MODULE_TAGS := optional
