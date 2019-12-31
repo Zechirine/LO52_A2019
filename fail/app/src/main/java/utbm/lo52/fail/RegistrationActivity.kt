f1e72c1d82898c27d1e6b0403da59d441107fdf5
Bartuccio Antoine <klmp200@klmp200.net>
2019-12-31 01:45:47 +0100

    project: now editable list of players for a given race

--- fail/app/src/main/java/utbm/lo52/fail/RegistrationActivity.kt
+++ fail/app/src/main/java/utbm/lo52/fail/RegistrationActivity.kt
@@ -15,11 +15,13 @@ import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.player_widget.view.*
 import kotlinx.android.synthetic.main.registration_activity.*
 import utbm.lo52.fail.db.*
+import java.util.*
+import kotlin.collections.ArrayList
 
 @ExperimentalStdlibApi
 class RegistrationActivity : AppCompatActivity() {
 
-    private val players = ArrayList<Player>()
+    private var players = LinkedList<Player>()
     private val teams = ArrayList<Team>()
 
     private lateinit var playerAdapter: PlayerAdapter
@@ -31,7 +33,7 @@ class RegistrationActivity : AppCompatActivity() {
         setContentView(R.layout.registration_activity)
 
         db = DBHelper(this, null)
-        playerAdapter = PlayerAdapter(db)
+        playerAdapter = PlayerAdapter(this, db)
 
         // This should not fail, normally
         race = db.request(Race::class).get(
@@ -44,38 +46,55 @@ class RegistrationActivity : AppCompatActivity() {
         if (!db.request(Team::class).filterRelated(Race::class, "id", race.id!!).exists())
             db.save(Team(null, "team 1", ForeignKey(Race::class, race.id)))
 
-        Log.v("pd", "raceid: ${race.id}, racename: ${race.name}")
         // Restore player list
         for (team in db.request(Team::class).filterRelated(
             Race::class,
             "id",
             race.id!!
         ).all() as List<Team>) {
-            Log.v("pd", "teamid: ${team.id}, teamname: ${team.name}")
             teams.add(team)
             for (player in db.request(Player::class).filter(
                 "team_id",
                 team.id!!
             ).all() as List<Player>) {
-                Log.v("pd", "playerid ${player.id}")
                 players.add(player)
             }
-        }
 
-        playerAdapter.submitList(players)
-        PlayerList.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        }
+        PlayerList.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         PlayerList.adapter = playerAdapter
+        playerAdapter.submitList(players)
 
         addPlayerButton.setOnClickListener {
             addPlayer()
         }
+
+        // If there is no player, there are as many default player as the number of team
+        if (players.isEmpty()) {
+            for (team in teams)
+                addPlayer()
+        }
+
     }
 
     private fun addPlayer() {
         val player = db.save(Player(null, "", 0, ForeignKey(Team::class, teams[0].id!!))) as Player
+        players = LinkedList(players)
         players.add(player)
         playerAdapter.submitList(players)
+//        playerAdapter.notifyDataSetChanged()
         Log.v("players", players.toString())
+    }
+
+    fun removePlayer(id: Int?) {
+        val newPlayers = LinkedList<Player>()
+        for (player in players) {
+            if (player.id != id) {
+                newPlayers.add(player)
+            }
+        }
+        players = newPlayers
+        playerAdapter.submitList(players)
     }
 
     override fun onSaveInstanceState(outState: Bundle?) {
@@ -87,16 +106,17 @@ class RegistrationActivity : AppCompatActivity() {
 
 
 @UseExperimental(ExperimentalStdlibApi::class)
-class PlayerAdapter(val db: DBHelper) :
+class PlayerAdapter(val activity: RegistrationActivity, val db: DBHelper) :
     ListAdapter<Player, PlayerAdapter.PlayerViewHolder>(DiffCallback()) {
 
     @ExperimentalStdlibApi
-    class PlayerViewHolder(val view: View, val db: DBHelper) : RecyclerView.ViewHolder(view) {
+    class PlayerViewHolder(val view: View, val activity: RegistrationActivity, val db: DBHelper) :
+        RecyclerView.ViewHolder(view) {
         fun bind(player: Player) {
             if (player.name != "") {
-                view.editText.setText(player.name)
+                view.playerNameText.setText(player.name)
             }
-            view.editText.addTextChangedListener(object : TextWatcher {
+            view.playerNameText.addTextChangedListener(object : TextWatcher {
                 override fun afterTextChanged(s: Editable?) {}
                 override fun beforeTextChanged(
                     s: CharSequence?,
@@ -111,13 +131,16 @@ class PlayerAdapter(val db: DBHelper) :
                     db.save(player)
                 }
             })
+            view.deletePlayerButton.setOnClickListener {
+                activity.removePlayer(player.id)
+            }
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlayerViewHolder {
         return PlayerViewHolder(
             LayoutInflater.from(parent.context)
-                .inflate(R.layout.player_widget, parent, false), db
+                .inflate(R.layout.player_widget, parent, false), activity, db
         )
     }
 
